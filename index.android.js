ed51c54e4fbc626062946ea39390c6f020a135d0
jack921 <355596943@qq.com>
2018-04-25 21:27:15 +0800

    init

--- index.android.js
+++ index.android.js
@@ -16,7 +16,9 @@ export default class HelloWorld extends React.Component {
     constructor(props){
         super(props);
         this.state={
-             text:'HelloJacks',
+             text1:'ToastForAndroid',
+             text2:'testAndroidCallbackMethod',
+             text3:'textAndroidPromiseMethod',
         }
     }
 
@@ -32,7 +34,13 @@ export default class HelloWorld extends React.Component {
      return (
           <View style={styles.container}>
            <TouchableOpacity onPress={this._onPressButton.bind(this)}>
-                 <Text style={styles.hello}>{this.state.text}</Text>
+                 <Text style={styles.hello}>{this.state.text1}</Text>
+           </TouchableOpacity>
+           <TouchableOpacity onPress={this._onPressButton2.bind(this)}>
+                 <Text style={styles.hello}>{this.state.text2}</Text>
+            </TouchableOpacity>
+           <TouchableOpacity onPress={this._onPressButton3.bind(this)}>
+                 <Text style={styles.hello}>{this.state.text3}</Text>
            </TouchableOpacity>
           </View>
         )
@@ -40,15 +48,21 @@ export default class HelloWorld extends React.Component {
 
    _onPressButton(){
         NativeModules.ToastForAndroid.show(1000);
-      // NativeModules.ToastForAndroid.testAndroidCallbackMethod("HelloJack",(result)=>{
-      //     this.setState({text:result});
-      // });
-      //   NativeModules.ToastForAndroid.textAndroidPromiseMethod("abcx").then((result)=>{
-      //       this.setState({text:result});
-      //   }).catch((error)=>{
-      //       this.setState({text:'error'});
-      //   })
    }
+
+    _onPressButton2(){
+        NativeModules.ToastForAndroid.testAndroidCallbackMethod("HelloJack",(result)=>{
+           this.setState({text:result});
+       });
+    }
+
+    _onPressButton3(){
+       NativeModules.ToastForAndroid.textAndroidPromiseMethod("abcx").then((result)=>{
+                 this.setState({text3:result});
+             }).catch((error)=>{
+                 this.setState({text:'error'});
+             })
+    }
 
 }
 
@@ -56,6 +70,7 @@ export default class HelloWorld extends React.Component {
      container: {
         flex: 1,
         justifyContent: 'center',
+        flexDirection: 'column',
       },
       hello: {
         fontSize: 20,
